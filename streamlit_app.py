56ea21e7898b6fd315fa1acfc246e006a235394a
pashaankim <pasharajbani.d1@gmail.com>
2026-05-22 23:13:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,16 @@ st.subheader("One", divider=True)
 st.subheader("Two", divider=True)
 st.subheader("Three", divider=True)
 st.subheader("Four", divider=True)
+import streamlit as st
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
